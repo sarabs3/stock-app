c1fee88c8a10c977ee0bd98a9e3ecb179913e01a
sarabjeet singh <sarabsingh212@gmail.com>
2021-07-04 18:52:35 +0530

    Show stats in dashboard

--- src/pages/dashboard/Index.js
+++ src/pages/dashboard/Index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import { DataStore } from '@aws-amplify/datastore';
+import { UserTrades } from '../../models';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import AppLayout from '../../components/layout/AppLayout';
+import _gropuBy from 'lodash/groupBy';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 function Copyright() {
   return (
@@ -102,7 +110,31 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function createStats (values) {
+  return Object.keys(values).map(val => ({
+    name: val,
+    trades: values[val].length,
+    quantity: values[val].reduce((a,c) => a + c.quantity, 0),
+    amount: values[val].reduce((a,c) => a + c.totalAmount, 0),
+  }))
+}
+
 export default function Dashboard() {
+  const [holdings, setHoldings] = useState([]);
+    
+  const fetchScripts = async () => {
+      const models = await DataStore.query(UserTrades);
+      return models;
+  }
+  useEffect(
+    () => {
+      fetchScripts().then(data => {
+        const groupValues = _gropuBy(data, (d) => d.Scrips.symbol);
+        setHoldings([...createStats(groupValues)]);
+      })
+    },
+    []
+  );
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   return (
@@ -125,6 +157,32 @@ export default function Dashboard() {
             <Grid item xs={12}>
               <Paper className={classes.paper}>
                 {/* <Orders trades={trades} /> */}
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                        <TableCell>Scrip Name</TableCell>
+                        <TableCell>Total Trades</TableCell>
+                        <TableCell>Total Quantity</TableCell>
+                        <TableCell>Price Per Share</TableCell>
+                        <TableCell>Total Amount</TableCell>
+                      </TableRow>
+                    </TableHead>
+                  <TableBody>
+                    {holdings.length > 0 && holdings.map(stat => (
+                      <TableRow key={stat.name}>
+                        <TableCell>{stat.name}</TableCell>
+                        <TableCell>{stat.trades}</TableCell>
+                        <TableCell>{stat.quantity}</TableCell>
+                        <TableCell>{(stat.amount / stat.quantity).toFixed(2)}</TableCell>
+                        <TableCell>{stat.amount}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
               </Paper>
             </Grid>
           </Grid>
